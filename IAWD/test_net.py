d8605a37aa085788ccbdf94bd2300a7f2b90f76d
qing-xue <964262601@qq.com>
2023-02-09 11:51:26 +0800

    test img dir

--- IAWD/test_net.py
+++ IAWD/test_net.py
@@ -1,7 +1,9 @@
 from PIL import Image
+import os
 import torch
 import numpy as np
 import copy
+import pandas as pd
 
 from net import IAWD
 from IA_Features import IA_Features
@@ -18,11 +20,21 @@ def old2keys(state_dict):
     return state_dict_v2
 
 
+def process_input(im):
+    """将 PIL 图片处理为 Tensor 类型以适用于模型"""
+    (width, height) = (im.width // 2, im.height // 2)
+    im = im.resize((width, height))  # PIL 先 Resize 减少计算量
+    info = IA_Features().getInfo(im)
+    x = torch.from_numpy(np.array(info, dtype=np.float32))
+    x = torch.unsqueeze(x, 0)        # add dim [B, C, ...]
+    return x
+
+
 # TODO: Only for Beijing Dataset ------------------------------
 Daytime_PM_MAX = 262.0
 Daytime_PM_MIN = 1.0
 
-# -------------------------------------------------------------
+# 模型：-------------------------------------------------------------
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 ckp = torch.load(r'PM_IAWD_Beijing.pk', map_location=device)
 
@@ -31,19 +43,32 @@ state_dict = old2keys(ckp['model'])
 net.load_state_dict(state_dict, strict=True)
 net.eval()
 
-# -------------------------------------------------------------
-img_name = r'../imgs/Beijing_20190530051212642_PM=22.jpg'
-im = Image.open(img_name).convert('RGB')
+# 数据：图片所在文件夹 ---------------------------------------------
+test_imgs = r'D:\workplace\dataset\V-picture'
+img_dir = test_imgs + '/'
+output_dir = img_dir
+print("pred_dir:", output_dir)
 
-(width, height) = (im.width // 2, im.height // 2)
-im = im.resize((width, height))  # PIL 先 Resize 减少计算量
+# 开始测试：----------------------------------------------------------
+all_pred = np.empty((0, 1), float)
+img_names = []
 
-info = IA_Features().getInfo(im)
+for img_name in os.listdir(img_dir):
+    if not img_name.endswith(('jpg', 'png', 'jpeg')):
+        continue
+    print(f'\r {img_name}', end='\n', flush=True)
+    img_names += [img_name]
 
-# -------------------------------------------------------------
-with torch.no_grad():
-    x = torch.from_numpy(np.array(info, dtype=np.float32))
-    x = torch.unsqueeze(x, 0)  # add dim [B, C, ...]
-    pred = net(x)
-    pred_nor = pred * (Daytime_PM_MAX - Daytime_PM_MIN) + Daytime_PM_MIN
-    print("Estimated PM2.5: ", pred_nor.flatten())
+    im = Image.open(img_dir + img_name).convert('RGB')
+    x = process_input(im)
+
+    with torch.no_grad():
+        pred = net(x)
+        pred = torch.squeeze(pred.clamp(0, 1).cpu())
+        pred_nor = pred * (Daytime_PM_MAX - Daytime_PM_MIN) + Daytime_PM_MIN
+        print("Estimated PM2.5: ", pred_nor.flatten())
+
+    all_pred = np.vstack((all_pred, pred_nor))
+
+df = pd.DataFrame({'IMG': img_names, 'Preds': all_pred.flatten()})
+df.to_excel(os.path.join(output_dir, 'results.xlsx'), index=False)
--- MIFF/test_svr.py
+++ MIFF/test_svr.py
@@ -2,9 +2,22 @@ import os
 from PIL import Image
 from joblib import load
 import numpy as np
+import pandas as pd
 
-from config import PM_PATH
 from svr_prepare import f1f2f3
+
+
+def process_input(im):
+    """Feature extracting..."""
+    (width, height) = (im.width // 2, im.height // 2)
+    im = im.resize((width, height))  # PIL 先 Resize 减少计算量
+
+    rate, depth_sim, residual_sim = f1f2f3(im)
+    # 注意特征顺序：features = ['depth_sim', 'residual', 'rate']
+    valid_features = np.asarray([[depth_sim, residual_sim, rate]])
+    valid_features = (valid_features - SERIES_MIN) / (SERIES_MAX - SERIES_MIN)
+
+    return valid_features
 
 
 # Just for Beijing dataset ---------------
@@ -13,19 +26,30 @@ SCALE = PM_MAX - PM_MIN
 SERIES_MAX = np.asarray([0.993714, 0.432661, 1.019228])  # 顺序：depth_sim, residual, rate
 SERIES_MIN = np.asarray([0.859622, 0.039976, 0.967000])  # 顺序：depth_sim, residual, rate
 
-# Image -----------------------------------
-img_name = os.path.join(PM_PATH, r'imgs/Beijing_20190530051212642_PM=22.jpg')
-im = Image.open(img_name).convert('RGB')
-(width, height) = (im.width // 2, im.height // 2)
-im = im.resize((width, height))  # PIL 先 Resize 减少计算量
-
-print('Feature extracting...')
-rate, depth_sim, residual_sim = f1f2f3(im)
-# 注意特征顺序：features = ['depth_sim', 'residual', 'rate']
-valid_features = np.asarray([[depth_sim, residual_sim, rate]])
-valid_features = (valid_features - SERIES_MIN) / (SERIES_MAX - SERIES_MIN)
+# Image 目录-----------------------------------
+test_imgs = r'D:\workplace\dataset\V-picture'
+img_dir = test_imgs + '/'
+output_dir = img_dir
+print("pred_dir:", output_dir)
 
 # Test --------------------------------------------------
-bst = load('miff.joblib')
-preds = bst.predict(valid_features) * SCALE + PM_MIN
-print('Estimate: ', preds)
+bst = load('miff.joblib')  # TODO：模型
+all_pred = np.empty((0, 1), float)
+img_names = []
+
+for img_name in os.listdir(img_dir):
+    if not img_name.endswith(('jpg', 'png', 'jpeg')):
+        continue
+    print(f'\r {img_name}', end='\n', flush=True)
+    img_names += [img_name]
+
+    im = Image.open(img_dir + img_name).convert('RGB')
+    valid_features = process_input(im)
+
+    pred_nor = bst.predict(valid_features) * SCALE + PM_MIN
+    print('Estimate: ', pred_nor)
+
+    all_pred = np.vstack((all_pred, pred_nor))
+
+df = pd.DataFrame({'IMG': img_names, 'Preds': all_pred.flatten()})
+df.to_excel(os.path.join(output_dir, 'results.xlsx'), index=False)
